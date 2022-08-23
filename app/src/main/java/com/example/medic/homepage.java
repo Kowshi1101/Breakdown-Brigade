6a528edfd6f4c37258bbcd6992c51c79710c7435
Narendranath <rsnarendranath@gmail.com>
2022-08-23 20:20:31 +0530

    create pakage

--- app/src/main/java/com/example/medic/homepage.java
+++ app/src/main/java/com/example/medic/homepage.java
@@ -9,19 +9,37 @@ import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
 
-    Button btn;
+    Button btn_user,btn_foreman, btn_admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        this.btn=(Button)findViewById(R.id.button3);
+        this.btn_user=(Button)findViewById(R.id.button3);
+        this.btn_foreman=(Button)findViewById(R.id.button2);
+        this.btn_admin=(Button)findViewById(R.id.button);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(homepage.this, LoginActivity.class);
+                Intent intent = new Intent(homepage.this, user_login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btn_foreman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(homepage.this, foreman_login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btn_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(homepage.this, admin_login.class);
                 startActivity(intent);
                 finish();
             }

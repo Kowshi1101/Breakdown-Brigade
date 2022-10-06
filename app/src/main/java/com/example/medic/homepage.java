143770593240569b02e21def8fa24e5a27fa660f
Narendranath <rsnarendranath@gmail.com>
2022-10-07 00:12:08 +0530

    map working

--- app/src/main/java/com/example/medic/homepage.java
+++ app/src/main/java/com/example/medic/homepage.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class homepage extends AppCompatActivity {
 
-    Button btn_user,btn_foreman, btn_admin;
+    Button btn_user,btn_foreman;
+    TextView btn_admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class homepage extends AppCompatActivity {
 
         this.btn_user=(Button)findViewById(R.id.button3);
         this.btn_foreman=(Button)findViewById(R.id.button2);
+        this.btn_admin=(TextView)findViewById(R.id.textView4);
 
 
         btn_user.setOnClickListener(new View.OnClickListener() {
@@ -36,13 +39,13 @@ public class homepage extends AppCompatActivity {
                 finish();
             }
         });
-//        btn_admin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(homepage.this, admin_login.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
+        btn_admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(homepage.this, admin_login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }

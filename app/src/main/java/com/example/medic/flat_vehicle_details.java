a5d073b97ecc4c3af6a88c85d60de816736cf122
Narendranath <rsnarendranath@gmail.com>
2022-10-07 00:20:10 +0530

    map working

--- app/src/main/java/com/example/medic/flat_vehicle_details.java
+++ app/src/main/java/com/example/medic/flat_vehicle_details.java
@@ -2,13 +2,24 @@ package com.example.medic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class flat_vehicle_details extends AppCompatActivity {
-
+Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flat_vehicle_details);
+        button6 = findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(flat_vehicle_details.this,location.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/medic/lift_vehicle_details.java
+++ app/src/main/java/com/example/medic/lift_vehicle_details.java
@@ -2,13 +2,24 @@ package com.example.medic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class lift_vehicle_details extends AppCompatActivity {
-
+    Button button5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lift_vehicle_details);
+        button5 = findViewById(R.id.button4);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(lift_vehicle_details.this,location.class);
+                startActivity(intent);
+            }
+        });
     }
 }
